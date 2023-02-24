a9461544b1c583fb075d09d64cb81742a1b4ef24
Tara Denniston <taradenniston@gmail.com>
2023-02-24 07:39:47 -0500

    create model for cupcake and populate database

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from keys import SECRET_KEY
+from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 app.app_context().push()
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+
 
 
